b4fe711bfa9ff89abda0b0e384891fb34957a497
KevinOchiengg <kevinohkelvin9@gmail.com>
2021-10-05 19:13:16 +0300

    modified chat section

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }))
 console.log('sv' + process.env.MONGODB_URL)
 
 mongoose.connect(
-  process.env.MONGODB_URL || 'mongodb://localhost/integrowear',
+  'mongodb+srv://Kevin:cliveotieno9019$@cluster0.l3ae2.mongodb.net/integrowear?retryWrites=true&w=majority',
 
   {
     useNewUrlParser: true,
