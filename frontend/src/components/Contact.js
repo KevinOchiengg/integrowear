0093c72eff05141d43a21f624a7dd0c948e9ff52
KevinOchiengg <kevinohkelvin9@gmail.com>
2021-10-10 23:03:50 +0300

    created search field component

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -55,7 +55,7 @@ const Contact = () => {
 
         <iframe
           className='map'
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30153.788252261566!2d72.82321484621745!3d19.141690214227783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b63aceef0c69%3A0x2aa80cf2287dfa3b!2sJogeshwari%20West%2C%20Mumbai%2C%20Maharashtra%20400047!5e0!3m2!1sen!2sin!4v1633431163028!5m2!1sen!2sin'
+          src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Nakuru,%20Biashara+(Integrowears)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
           allowFullScreen=''
           loading='lazy'
         ></iframe>
